ff82d919816a7d6c59f28b0c915db39ce59e12d7
Jean Carlo da Silva <jeaan3@gmail.com>
2018-12-01 10:43:45 -0200

    initial commit

---
+++ hustle-modeTests/hustle_modeTests.swift
@@ -0,0 +1,34 @@
+//
+//  hustle_modeTests.swift
+//  hustle-modeTests
+//
+//  Created by Jean Carlo da Silva on 23/10/18.
+//  Copyright © 2018 Jean Carlo da Silva. All rights reserved.
+//
+
+import XCTest
+@testable import hustle_mode
+
+class hustle_modeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
